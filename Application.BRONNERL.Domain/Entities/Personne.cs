ffdfee03d17156f1c5dc4cc67effda2b750130c0
LBRONNER <LBR@dimosoftware.com>
2022-08-01 21:27:01 +0200

    gestion EF core

--- Application.BRONNERL.Domain/Entities/Personne.cs
+++ Application.BRONNERL.Domain/Entities/Personne.cs
@@ -1,9 +1,12 @@
 using Application.BRONNERL.Domain.Interfaces.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.BRONNERL.Domain.Entities
 {
     public class Personne : IAgeable
     {
+        [Key]
+        public int Id { get; set; }
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public DateTime DateDeNaissance { get; set; }
---
+++ Application.BRONNERL.Persistence/Migrations/20220801192322_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.BRONNERL.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personnes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateDeNaissance = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personnes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personnes");
+        }
+    }
+}
